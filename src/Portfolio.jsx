44fd04eeed7db11f33a3fa0915994d95cd5e9c55
Ranjith Katta <ranjithkatta220@gmail.com>
2025-07-02 12:09:23 -0500

    fix: added missing Portfolio.jsx file

---
+++ src/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Ranjith Katta | UI/UX Designer</h1>
+      {/* ...rest of your portfolio JSX code here... */}
+    </div>
+  );
+}
